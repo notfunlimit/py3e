062196ed7290423984d82e6857d3d702639fd595
XDASADX <leemailbox1@gmail.com>
2014-12-21 18:31:16 +0800

    An update
    fixed some problem.

--- process_frequency.py
+++ process_frequency.py
@@ -5,7 +5,7 @@ def efficiency(x):
 	for i in range(1,x):
 		pass
 	T_II=time.time()
-	print(T_II-T_I)
+	return (T_II-T_I)
 
 def efficiency_c(x):
 	T_I=time.strftime('%M:%S',time.localtime(time.time()))
@@ -21,14 +21,26 @@ def efficiency_avr(x):
 	for i in range(1,x):
 		pass
 	T_II=time.time()
-	print((T_II-T_I)/x)
+	return ((T_II-T_I)/x)
 
 
 def frequency(x):
 #bigger is better
+#unit:MHz
 	T_I=time.time()
 	for i in range(1,x):
 		pass
 	T_II=time.time()
-	print((1/((T_II-T_I)/x))/1000000)
-	print('MHz')
+	return ((1/((T_II-T_I)/x))/1000000)
+
+def frequency_csv(x):
+        file=open(r'C:\result.csv','w')
+        for ii in range(1000000,1000000+x):
+                resultx=str(frequency(ii))
+                file.write(resultx+'\n')
+        file.write('=AVERAGE(A1:A'+str(x)+')')
+        file.close()
+        return 'finished'
+
+
+

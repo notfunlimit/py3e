58c670767146e36d7c975caa8a1e2232addb2d7b
XDASADX <leemailbox1@gmail.com>
2015-01-01 17:11:32 +0800

    #3

--- xbenchmark_main_fuction.py
+++ xbenchmark_main_fuction.py
@@ -1,6 +1,7 @@
 import time
 import os
 from tkinter import *
+import threading
 
 def frequency(x):
 #bigger is better
@@ -11,18 +12,28 @@ def frequency(x):
 	T_II=time.time()
 	return ((1/((T_II-T_I)/x))/1000000)
 
-def frequency_csv(x):
+
+
+def make_csv(x):
+
+        #create address
         if os.path.exists(r'D:\XBENCHMARK\Result')==1:
                 pass
         else:
                 os.mkdir(r'D:\XBENCHMARK\Result')
+        
+        #start
         T_A=time.time()
         print('Benchmark started at:'+time.strftime('%Y-%m-%d-%H:%M:%S'))
         file=open(r'D:\XBENCHMARK\Result\result-'+time.strftime('%Y-%m-%d-%H-%M-%S')+'#'+str(int(x))+'.csv','w')
         
+        #main
         for ii in range(1,int(x)):
                 resultx=frequency(1000000)
                 file.write(str(resultx)+'\n')
+                print(str(ii)+'/1000')
+
+        #handle result
         file.write('##################STATS#################\n')
         file.write('Average:'+','+'=AVERAGE(A1:A'+str(int(x))+')'+',MHz \n')
         file.write('At'+','+time.strftime('%Y-%m-%d-%H:%M:%S')+'\n')
@@ -31,14 +42,13 @@ def frequency_csv(x):
         file.write('In'+','+str(T_B-T_A)+',secs.')
         file.write('\n########################################')
         file.close()
+
+        #feedback
         print('In '+str(T_B-T_A)+' secs.')
         print('Result saved at:'+'D:\XBENCHMARK\Result')
 
-
-
 if __name__=='__main__':
         #num=input('Takes:')
-        frequency_csv(1000)
-
+        make_csv(1000)
 
 

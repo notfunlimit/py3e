faee6bc8b6ad7db6ee7b424ce574ee25b7b0c13b
XDASADX <leemailbox1@gmail.com>
2015-01-03 13:43:37 +0800

    #2
    added automatic selections

--- main.py
+++ main.py
@@ -7,8 +7,19 @@ local_addr=os.getcwd()
 sys.path.append(local_addr+r'\func')
 
 #import functions
-import gene_result
+import gnrs
 
-
+#main
 if __name__ == '__main__':
-	gene_result.make_csv(1000)
+	#auto_select takes
+	tks=gnrs.frequency(1000000)
+	if tks in range(0,10):
+		gnrs.make_csv(50)
+	elif tks in(10,20):
+		gnrs.make_csv(100)
+	elif tks in range(20,30):
+		gnrs.make_csv(200)
+	elif tks in range(30,40):
+		gnrs.make_csv(400)
+	elif tks >= 40:
+		gnrs.make_csv(500)

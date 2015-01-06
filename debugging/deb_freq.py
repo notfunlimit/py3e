57e067c418884e80498d458e7a54ed4d91d72b99
XDASADX <leemailbox1@gmail.com>
2015-01-06 20:16:42 +0800

    Update #1
    code of:
    gnrs.frequency()

---
+++ debugging/deb_freq.py
@@ -0,0 +1,19 @@
+#I.S.M.
+import sys
+import os
+
+#define new address
+func=['f','u','n','c']
+
+#A.S.L.A.
+local_addr=list(os.getcwd())
+local_addr=''.join(local_addr[:-5]+func)
+sys.path.append(local_addr)
+
+#I.L.F.
+import gnrs
+
+#D.C.
+if __name__ == '__main__':
+	gnrs.deb_inst_freq(20)
+	os.system('cls')

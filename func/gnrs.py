6918d613f83bd4cf983732292286c2d7f7652f60
XDASADX <leemailbox1@gmail.com>
2015-01-07 20:06:27 +0800

    update #14
    optimized the code

--- func/gnrs.py
+++ func/gnrs.py
@@ -7,6 +7,9 @@ import time
 import os
 from tkinter import *
 
+loc_addr=os.getcwd()
+csv_save_addr=loc_addr+r'\result'
+
 #main
 #part I
 def frequency(x):
@@ -25,40 +28,40 @@ def make_csv(x):
         total=0
 
         #create local address
-        if os.path.exists(r'D:\XBENCHMARK\Result')==1:
+        if os.path.exists(csv_save_addr)==1:
                 pass
         else:
-                os.mkdir(r'D:\XBENCHMARK\Result')
+                os.mkdir(csv_save_addr)
         
         #start
         T_A=time.time()
         print('Benchmark started at:'+time.strftime('%Y-%m-%d-%H:%M:%S'))
-        file=open(r'D:\XBENCHMARK\Result\result-'+time.strftime('%Y-%m-%d-%H-%M-%S')+'#'+str(int(x))+'.csv','w')
+        file=open(csv_save_addr+'\\result-'+time.strftime('%Y-%m-%d-%H-%M-%S')+'#'+str(int(x))+'.csv','w')
         
         #main
         for ii in range(1,int(x)):
-                resultx=frequency(1000000)
+                resultx=frequency(100000)
                 file.write(str(resultx)+'\n')
                 print(str(ii)+'/'+str(int(x)))
                 total+=resultx
         print(str(int(x))+'/'+str(int(x)))
         
         #file feedback
-        file.write('########################################\n')
+        file.write('#'*40+'\n')
         file.write('Average:'+','+'=AVERAGE(A1:A'+str(int(x))+')'+',MHz \n')
         file.write('At'+','+time.strftime('%Y-%m-%d-%H:%M:%S')+'\n')
         T_B=time.time()
         file.write('In'+','+str(T_B-T_A)+',secs.')
-        file.write('\n########################################')
+        file.write('\n'+'#'*40)
         file.close()
 
         #feedback
         print('Benchmark ended at:'+time.strftime('%Y-%m-%d-%H:%M:%S'))
-        print('Result saved at:'+'D:\XBENCHMARK\Result')
-        print('########################################')
+        print('Result saved at:'+csv_save_addr)
+        print('#'*40)
         print('In '+str(T_B-T_A)+' secs.')
         print('Average:'+str(total/int(x))+'MHz')
-        print('########################################')
+        print('#'*40)
 
 #part III intitalizing
 def intitial(x):
@@ -75,7 +78,7 @@ def intitial(x):
 def deb_inst_avr(x):
     tl=0
     for i in range(1,x):
-        tl+=frequency(1000000)
+        tl+=frequency(100000)
     t_a=tl/x
     return t_a
 

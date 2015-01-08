f8daaeeb6f581a505746738bf960bc959b977f65
XDASADX <leemailbox1@gmail.com>
2015-01-08 19:57:40 +0800

    Update #2
    added slope fomula

--- libs/mathlib.py
+++ libs/mathlib.py
@@ -18,11 +18,11 @@ class algebra:
 				n*=i
 			return n
 		else:
-			return('Invaild Number')
+			return('Invaild input')
 
 class analytic_geometry:
 	#fomula of distance between two points
-	#pint_I is a list of a point such as[x1,y1] ,pint_II is a list of[x2,y2]
+	#pint_I is a list of a point such as[x1,y1] ,pint_II is a list such as[x2,y2]
 	#√((x2-x1)^2+(y2-y1)^2))
 	def dis_t_points(pint_I,pint_II):
 		return sqrt(((pint_II[0]-pint_I[0])**2)+((pint_II[1]-pint_I[1])**2))
@@ -32,3 +32,15 @@ class analytic_geometry:
     #abs(a*x1+b*y1+c)/sqrt(a^2+b^2)
 	def dis_line_point(pint,line):
 		return abs(line[0]*pint[0]+line[1]*pint[1]+line[2])/sqrt(line[0]**2+line[1]**2)
+
+	#slope fomula
+	#var_I and var_II are list or float such as [x1,y1] and [x2,y2] or a and b (ax+by+c=1)
+	#(y2-y1)/(x2-x1) or -a/c
+	def slope(var_I,var_II):
+		if type(var_I) and type(var_II) == type(1):
+			return(-var_I/var_II)
+		elif type(var_I) and type(var_II) == type([1,1]):
+			return (var_II[1]-var_I[1])/(var_II[0]-var_I[0])
+		else:
+			return 'Invaild input'
+		

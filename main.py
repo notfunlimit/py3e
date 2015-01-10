00c5fd164e3052eefdc49120ad1549216d2bc827
XDASADX <leemailbox1@gmail.com>
2015-01-10 17:44:49 +0800

    Update #13
    Optimized code and added new module "run"

--- main.py
+++ main.py
@@ -16,24 +16,11 @@ sys.path.append(loc_addr)
 import adsys
 
 adsys.add_sys()
+import gnrs
 
 #import functions
 import run
 
 #main
 if __name__ == '__main__':
-
-    frame_main=Tk()
-    frame_main.iconbitmap('res/icon/ICO_32x32.ico')
-    frame_main.title("Pyth0n3c1ipse")
-    frame_main.geometry('256x32')
-    frame_main.resizable(False, False)
-    def rproc():
-    	run.mcsv_run()
-    	for i in range(0,200):
-    		status=Label(frame_main,text=str(i)+'/'+'200')
-    		status.pack(fill=X,side='left')
-    process = Button(frame_main,text='RUN_BENCHMARK',command=rproc,activeforeground='blue',activebackground='white')
-    process.pack(fill=Y,side='right')
-    mainloop()
-
+    gnrs

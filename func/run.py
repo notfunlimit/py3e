2ee8c500843dfd5148d8f8816c88cb4f1f4149f3
XDASADX <leemailbox1@gmail.com>
2015-01-11 15:08:23 +0800

    Update run.py
    Compatible with class'benchmark'

--- func/run.py
+++ func/run.py
@@ -2,14 +2,14 @@ import gnrs
 
 def mcsv_run():
 	#intitialize
-	t_avr=gnrs.intitial(10)
+	t_avr=gnrs.benchmark.intitial(10)
 	#Automatics
 	
 	if t_avr in range(0,8):
-		gnrs.make_csv(80)
+		gnrs.benchmark.make_csv(80)
 	elif t_avr in range(8,20):
-		gnrs.make_csv(100)
+		gnrs.benchmark.make_csv(100)
 	elif t_avr > 20:
-		gnrs.make_csv(200)
+		gnrs.benchmark.make_csv(200)
 	else:
-		gnrs.make_csv(100)
+		gnrs.benchmark.make_csv(100)
